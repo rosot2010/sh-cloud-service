4629e0e6a28a72c2a04ac8dc9418feece9a01fbd
Henry <rosot2013@gmail.com>
2024-11-29 14:23:48 +0000

    first commit

---
+++ header.php
@@ -0,0 +1,71 @@
+	<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Buy good products on buy right">
+<link href="css/bootstrap.min.css" rel="stylesheet"/> 
+<link href="css/styles.css" rel="stylesheet"/> 
+  <meta name=”robots” content=”index, follow”>
+<meta property="og:type" content="Goods Website" />
+
+<meta property="og:title" content="Buy right" />
+<meta property="og:url" content="PERMALINK" />
+
+<meta property="og:site_name" content="Buy right" />
+</head>
+  
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="cookie_note.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+            <title>Buy right</title>
+              <link href="css/styles.css" rel="stylesheet" type="text/css">
+              
+  
+              <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+ 
+              
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         
+            <!-- Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		
+</head>
+<header class="container-fluid">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+	  data-target="#theNav"
+	  aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class="navbar-brand" href="index.php">
+	Buy Right
+		</a>
+      </div>
+    <div class="collapse navbar-collapse" id="theNav">
+        <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="index.php">Home</a></li>
+            
+
+
+</ul>
+
+      
+  </div>
+    </div>
+    </nav>
+</header>
